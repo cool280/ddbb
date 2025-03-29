3f4d63280d08724c1bf5f410f9d54f917fc75e6a
jiangxl29102 <20819109@qq.com>
2025-03-30 03:02:58 +0800

    c

--- src/main/java/com/ddbb/controller/LoginController.java
+++ src/main/java/com/ddbb/controller/LoginController.java
@@ -3,18 +3,20 @@ package com.ddbb.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.ddbb.annotate.DdbbController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @DdbbController
+@RequestMapping("/login")
 public class LoginController extends BaseController{
     @PostMapping("/sendSmsCode")
     @ResponseBody
     public JSONObject sendSmsCode(String mobile){
         return SUCCESS;
     }
-    @PostMapping("/login")
+    @PostMapping("/doLogin")
     @ResponseBody
     public JSONObject login(String mobile,String vCode){
         return SUCCESS;
--- src/main/java/com/ddbb/controller/NearbyController.java
+++ src/main/java/com/ddbb/controller/NearbyController.java
@@ -1,30 +1,34 @@
 package com.ddbb.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.ddbb.annotate.DdbbController;
 import com.ddbb.controller.request.NearbyAssistantCoachRequest;
 import com.ddbb.controller.request.NearbyAssistantCoachResponse;
 import com.ddbb.controller.request.NearbyHallRequest;
 import com.ddbb.controller.request.NearbyHallResponse;
 import com.ddbb.service.nearby.NearbyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@DdbbController
+@RequestMapping("/nearby")
 public class NearbyController extends BaseController {
     @Autowired
     private NearbyService nearbyService;
 
     @ResponseBody
-    public JSONObject getNearbyAssistantCoach(NearbyAssistantCoachRequest nearbyRequest){
+    @PostMapping("/coach")
+    public JSONObject getNearbyAssistantCoach(@RequestBody NearbyAssistantCoachRequest nearbyRequest){
         List<NearbyAssistantCoachResponse> ret = nearbyService.getNearbyAssistantCoach(nearbyRequest);
         return buildOk(ret,"");
     }
 
     @ResponseBody
-    public JSONObject getNearbyHall(NearbyHallRequest nearbyRequest){
+    @PostMapping("/hall")
+    public JSONObject getNearbyHall(@RequestBody NearbyHallRequest nearbyRequest){
         List<NearbyHallResponse> ret = nearbyService.getNearbyHall(nearbyRequest);
         return buildOk(ret,"");
     }
