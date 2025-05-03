17390ae69b40e283b5842eeeee3f217879bcf106
jiangxl29102 <20819109@qq.com>
2025-05-04 00:54:52 +0800

    test

---
+++ ddbb-web/src/test/java/com/ddbb/test/BaseTest.java
@@ -0,0 +1,76 @@
+package com.ddbb.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * java-springboot下使用websocket运行单侧时报异常：javax.websocket.server.ServerContainer not available
+ * 一：原因：spring boot内带tomcat，tomcat中的websocket会有冲突出现问题
+ * 二：解决方法：
+ *
+ * 1. 为SpringbootTest注解指定参数classes和webEnvironment：@SpringBootTest(classes = WebsocketServerTestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+ *
+ * 或者
+ *
+ * 2. 指定webEnvironment也可以：@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+ * 三：解析
+ *
+ * 因为WebSocket是servlet容器所支持的，所以需要加载servlet容器：
+ * webEnvironment参数为springboot指定ApplicationContext类型。
+ * webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT表示内嵌的服务器将会在一个随机的端口启动。
+ * webEnvironment 主要的值可以参考SpringbootTest源码：
+ */
+///**
+// * An enumeration web environment modes.
+// */
+//enum WebEnvironment {
+//
+//    /**
+//     * Creates a {@link WebApplicationContext} with a mock servlet environment if
+//     * servlet APIs are on the classpath, a {@link ReactiveWebApplicationContext} if
+//     * Spring WebFlux is on the classpath or a regular {@link ApplicationContext}
+//     * otherwise.
+//     */
+//    MOCK(false),
+//
+//    /**
+//     * Creates a web application context (reactive or servlet based) and sets a
+//     * {@code server.port=0} {@link Environment} property (which usually triggers
+//     * listening on a random port). Often used in conjunction with a
+//     * {@link LocalServerPort} injected field on the test.
+//     */
+//    RANDOM_PORT(true),
+//
+//    /**
+//     * Creates a (reactive) web application context without defining any
+//     * {@code server.port=0} {@link Environment} property.
+//     */
+//    DEFINED_PORT(true),
+//
+//    /**
+//     * Creates an {@link ApplicationContext} and sets
+//     * {@link SpringApplication#setWebApplicationType(WebApplicationType)} to
+//     * {@link WebApplicationType#NONE}.
+//     */
+//    NONE(false);
+//
+//    private final boolean embedded;
+//
+//    WebEnvironment(boolean embedded) {
+//        this.embedded = embedded;
+//    }
+//
+//    /**
+//     * Return if the environment uses an {@link ServletWebServerApplicationContext}.
+//     * @return if an {@link ServletWebServerApplicationContext} is used.
+//     */
+//    public boolean isEmbedded() {
+//        return this.embedded;
+//    }
+//
+//}
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@RunWith(SpringRunner.class)
+public class BaseTest {
+}
---
+++ ddbb-web/src/test/java/com/ddbb/test/challenge/ChallengeTest.java
@@ -0,0 +1,80 @@
+package com.ddbb.test.challenge;
+
+import com.ddbb.infra.data.mongo.repo.ChallengeRepo;
+import com.ddbb.internal.utils.ObjectConverter;
+import com.ddbb.service.challenge.ChallengeConfig;
+import com.ddbb.service.challenge.ChallengeKit;
+import com.ddbb.service.challenge.ChallengeScheduleDO;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Slf4j
+public class ChallengeTest {
+    @Autowired
+    private ChallengeConfig challengeConfig;
+    @Autowired
+    private ChallengeKit challengeKit;
+    @Resource
+    private ChallengeRepo challengeRepo;
+
+    @Test
+    public void testConfig(){
+        System.out.println("afterDays: "+challengeConfig.getAfterDays());
+    }
+    @Test
+    public void testGetOccupationTime(){
+        Map<String, List<Integer>> map = challengeKit.getOccupationTime(()->challengeRepo.getLaunchedTodayAndAfterChallenge(134L));
+        System.out.println(ObjectConverter.o2s(map));
+        map = challengeKit.getOccupationTime(()->challengeRepo.getReceivedTodayAndAfterChallenge(134L));
+        System.out.println(ObjectConverter.o2s(map));
+    }
+
+    @Test
+    public void testIsFree(){
+        int start = 0;
+        int end = 10;
+        boolean b = challengeKit.isFree(134L,"2025-04-13",start,end);
+        System.out.println(start+" - "+end+" is free: "+b);
+
+
+        start = 0;
+        end = 14;
+        b = challengeKit.isFree(134L,"2025-04-13",start,end);
+        System.out.println(start+" - "+end+" is free: "+b);
+
+        start = 0;
+        end = 15;
+        b = challengeKit.isFree(134L,"2025-04-13",start,end);
+        System.out.println(start+" - "+end+" is free: "+b);
+
+        start = 14;
+        end = 19;
+        b = challengeKit.isFree(134L,"2025-04-13",start,end);
+        System.out.println(start+" - "+end+" is free: "+b);
+
+        start = 17;
+        end = 19;
+        b = challengeKit.isFree(134L,"2025-04-13",start,end);
+        System.out.println(start+" - "+end+" is free: "+b);
+
+        start = 22;
+        end = 24;
+        b = challengeKit.isFree(134L,"2025-04-13",start,end);
+        System.out.println(start+" - "+end+" is free: "+b);
+    }
+    @Test
+    public void testGetSchedule(){
+        Map<String, ChallengeScheduleDO> schedule = challengeKit.getSchedule(134L);
+        System.out.println(ObjectConverter.o2s(schedule));
+    }
+}
