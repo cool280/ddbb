b75f1ad4e396ac1cbdf1e63c09efb3f8591d9e47
jiangxl29102 <20819109@qq.com>
2025-06-28 19:33:32 +0800

    a

---
+++ ddbb-ext-api/src/main/java/com/ddbb/extapi/wx/WxPayConstants.java
@@ -0,0 +1,6 @@
+package com.ddbb.extapi.wx;
+
+public class WxPayConstants {
+    public static String mainDomain = "https://api.mch.weixin.qq.com";
+    public static String standByDomain = "https://api.mch.weixin.qq.com";
+}
