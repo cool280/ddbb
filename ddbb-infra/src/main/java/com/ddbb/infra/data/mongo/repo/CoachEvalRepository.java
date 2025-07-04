7e40fe8a4da5648767e2831d72ec0e1d8dbb9977
Y <123@qq.com>
2025-07-04 22:11:13 +0800

    add order

--- ddbb-infra/src/main/java/com/ddbb/infra/data/mongo/repo/CoachEvalRepository.java
+++ ddbb-infra/src/main/java/com/ddbb/infra/data/mongo/repo/CoachEvalRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CoachEvalRepository extends MongoRepository<CoachEvalEntity, Long> {
 
-    CoachEvalEntity findByOrderId(Long orderId);
+//    CoachEvalEntity findByOrderId(Long orderId);
 
 }
